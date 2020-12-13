7ee327910b01b7918627ea4c4f3ad5c738a8a4ad
ty802 <kiany@thatnameisnotavailable.com>
2020-12-13 01:03:16 -0600

    i think i fixed it :)

--- index.js
+++ index.js
@@ -6,14 +6,11 @@ const fs = require('fs')
 const Privkey = fs.readFileSync(process.env.Privkeyfile)
 const cert = fs.readFileSync(process.env.certfile)
 let creds = {key:Privkey,cert:cert}
-var app = express(creds)
-app.listen(443)
-app.get('/',(req,res)=>{res.send('helloWorld!')})
-server = https.createServer(creds).listen(11256,()=>{console.log("[INIT] Https listing")})
-wss = new ws.Server({server:server})
-wss.on('connection',(e)=>{onConnect(e)})
+wshttps = https.createServer(creds)
+const wss = new ws.Server({wshttps})
+wss.on('connection',(socket)=>onConnect(socket))
 function onConnect(socket){
-socket.send('Hello World!')
-socket.terminate()
-
+    socket.send('hello world')
+    socket.terminate()
 }
+wshttps.listen(11256,()=>{console.log("[INIT] Https listing")})
