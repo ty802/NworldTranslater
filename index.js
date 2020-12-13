ba2770a8f3778cb6cfbee430b25ac9a2a3cc7801
ty802 <kiany@thatnameisnotavailable.com>
2020-12-13 00:33:25 -0600

    https test 1

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const { v4:uuidv4 } = require('uuid')
 const ws = require("ws")
-const http = require("http")
+const https = require("https")
 const fs = require('fs')
 const Privkey = fs.readFileSync(process.env.Privkeyfile)
 const cert = fs.readFileSync(process.env.certfile)
 let creds = {key:Privkey,cert:cert}
 var app = express(creds)
-server = app.listen(11256,()=>{console.log("[INIT] Https listing")})
-wss = new WebSocket.server(server)
+app.listen(443)
+app.get('/',(req,res)=>{res.send('helloWorld!')})
+server = https.createServer(creds).listen(11256,()=>{console.log("[INIT] Https listing")})
+wss = new ws.Server(server)
 wss.on('connection',(e)=>{onConnect(e)})
 function onConnect(socket){
 socket.send('Hello World!')
 socket.terminate()
+
 }
